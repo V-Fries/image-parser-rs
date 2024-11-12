21ccb154805ac2d04e9eea7c93aa1b80d972808b
Vincent Fries <github.herd7@aleeas.com>
2024-11-12 14:39:39 +0100

    feat: Image struct

---
+++ src/image.rs
@@ -0,0 +1,96 @@
+use std::fmt::Debug;
+
+pub const DEFAULT_ALPHA_VALUE: u8 = 0;
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct Rgba {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+    pub a: u8,
+}
+
+#[repr(C)]
+#[derive(Clone, Copy, Eq)]
+pub union Pixel {
+    pub rgba: Rgba,
+    pub color: u32,
+}
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct Image {
+    data: Box<[Pixel]>,
+
+    width: usize,
+    height: usize,
+}
+
+impl Image {
+    pub fn new(width: usize, height: usize, data: impl Into<Box<[Pixel]>>) -> Self {
+        let data = data.into();
+
+        assert!(
+            width
+                .checked_mul(height)
+                .expect("Image::new() width * height overflowed")
+                == data.len()
+        );
+
+        Self {
+            width,
+            height,
+            data,
+        }
+    }
+
+    pub fn width(&self) -> usize {
+        self.width
+    }
+
+    pub fn height(&self) -> usize {
+        self.height
+    }
+}
+
+impl Pixel {
+    pub fn color(&self) -> u32 {
+        unsafe { self.color }
+    }
+
+    pub fn rgba(&self) -> Rgba {
+        unsafe { self.rgba }
+    }
+
+    pub fn color_mut(&mut self) -> &mut u32 {
+        unsafe { &mut self.color }
+    }
+
+    pub fn rgba_mut(&mut self) -> &mut Rgba {
+        unsafe { &mut self.rgba }
+    }
+}
+
+impl PartialEq for Pixel {
+    fn eq(&self, other: &Self) -> bool {
+        self.color() == other.color()
+    }
+}
+
+impl Debug for Pixel {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Pixel {{ color: {} }}", self.color())
+    }
+}
+
+impl From<u32> for Pixel {
+    fn from(color: u32) -> Self {
+        Self { color }
+    }
+}
+
+impl From<Rgba> for Pixel {
+    fn from(rgba: Rgba) -> Self {
+        Self { rgba }
+    }
+}
